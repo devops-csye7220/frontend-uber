63427604f83961310e4436e67c180462d5b62a01
SajalSood <sajal.sood@gmail.com>
2021-04-24 20:04:57 -0400

    frontend cd

---
+++ build/precache-manifest.2d9755f5db372891cd799db2592cb3b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f60d7884ef3577db18be90d05f19b47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe0ddf3c9f83a114f256",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "07950e3620f5d5521622",
+    "url": "/static/css/main.d4794a9f.chunk.css"
+  },
+  {
+    "revision": "fe0ddf3c9f83a114f256",
+    "url": "/static/js/2.3ffc7dae.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/static/js/2.3ffc7dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07950e3620f5d5521622",
+    "url": "/static/js/main.311a745a.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "9fd42505808accd51a2537a2a5638a9b",
+    "url": "/static/media/landing-page-dashboard.9fd42505.jpeg"
+  },
+  {
+    "revision": "d66fc8229ccb24fbb72c978672fa6715",
+    "url": "/static/media/landing-page.d66fc822.jpeg"
+  }
+]);
