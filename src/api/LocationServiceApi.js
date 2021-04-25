f7d1df8e68dbecddca4fb9cd204f57149adca743
SajalSood <sajal.sood@gmail.com>
2021-04-24 20:11:36 -0400

    remove api server path

--- src/api/LocationServiceApi.js
+++ src/api/LocationServiceApi.js
@@ -1,7 +1,7 @@
 /* Location service APIs */
 import axios from 'axios';
 require('dotenv').config();
-const api_url = process.env.REACT_APP_SERVER_URL + "/api/locations" || "http://localhost:3001/api/locations";
+const api_url = "/api/locations" || "http://localhost:3001/api/locations";
 
 class LocationServiceApi {
     getAllLocations() {
